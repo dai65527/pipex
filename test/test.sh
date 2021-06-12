4139683c26a8c1f3294e24626eaa10fa28159671
dnakano <dnakano@student.42tokyo.jp>
2021-06-12 09:30:00 +0900

    update test

--- test/test.sh
+++ test/test.sh
@@ -9,6 +9,8 @@ if [ ! -x ../pipex ]; then
     make pipex -C .. 
 fi
 
+RETURNVAL=0
+
 echo "CASE: case1.txt cat cat out.txt" | tee -a test.log
 < case1.txt cat | cat > out_ref.txt
 RET_REF=$?
@@ -19,12 +21,14 @@ if [ $RET_REF = $RET_MINE ]; then
 else
     echo "  return val KO:)" | tee -a test.log
     echo "REF: $RET_REF, MINE: $RET_MINE" >> test.log
+    RETURNVAL=1
 fi
 diff out_ref.txt out_mine.txt >> test.log
 if [ $? -eq 0 ]; then
     echo "        diff OK:)" | tee -a test.log
 else
     echo "        diff KO:)" | tee -a test.log
+    RETURNVAL=1
 fi
 
 echo "CASE: case1.txt cat \"grep k\" out.txt" | tee -a test.log
@@ -37,12 +41,14 @@ if [ $RET_REF = $RET_MINE ]; then
 else
     echo "  return val KO:)" | tee -a test.log
     echo "REF: $RET_REF, MINE: $RET_MINE" >> test.log
+    RETURNVAL=1
 fi
 diff out_ref.txt out_mine.txt >> test.log
 if [ $? -eq 0 ]; then
     echo "        diff OK:)" | tee -a test.log
 else
     echo "        diff KO:)" | tee -a test.log
+    RETURNVAL=1
 fi
 
 echo "CASE: case1.txt cat \"grep z\" out.txt" | tee -a test.log
@@ -55,10 +61,34 @@ if [ $RET_REF = $RET_MINE ]; then
 else
     echo "  return val KO:)" | tee -a test.log
     echo "REF: $RET_REF, MINE: $RET_MINE" >> test.log
+    RETURNVAL=1
 fi
 diff out_ref.txt out_mine.txt >> test.log
 if [ $? -eq 0 ]; then
     echo "        diff OK:)" | tee -a test.log
 else
     echo "        diff KO:)" | tee -a test.log
+    RETURNVAL=1
 fi
+
+echo "CASE: case1.txt \"grep a\" \"wc\" out.txt" | tee -a test.log
+< case1.txt grep a | wc > out_ref.txt
+RET_REF=$?
+../pipex case1.txt "grep z" wc out_mine.txt
+RET_MINE=$?
+if [ $RET_REF = $RET_MINE ]; then
+    echo "  return val OK:)" | tee -a test.log
+else
+    echo "  return val KO:)" | tee -a test.log
+    echo "REF: $RET_REF, MINE: $RET_MINE" >> test.log
+    RETURNVAL=1
+fi
+diff out_ref.txt out_mine.txt >> test.log
+if [ $? -eq 0 ]; then
+    echo "        diff OK:)" | tee -a test.log
+else
+    echo "        diff KO:)" | tee -a test.log
+    RETURNVAL=1
+fi
+
+exit $RETURNVAL
